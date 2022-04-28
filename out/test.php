ec0ebddb6c62ea560f05e14703c548892012cd99
lukas <luk.ruck@gmail.com>
2022-04-28 16:38:47 +0200

    correction commit

---
+++ out/test.php
@@ -0,0 +1,15 @@
+<html lang="en" class="js canvas canvastext webgl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Haxe Application</title>
+</head>
+<body>
+
+<canvas tabindex="0" id="canvas" width="1423" height="650" style="outline: none; width: 1423px; height: 650px; cursor: auto;"></canvas>
+
+<script crossorigin="anonymous" type="text/javascript" src="js/test.js"></script>
+
+
+</body>
+</html>
